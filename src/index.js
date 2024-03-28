1a6115c2842660f648244c9ee915228916b60167
ExpertzZ <74832302+ExpertzZ@users.noreply.github.com>
2024-03-29 00:30:58 +0100

    use navigation bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import { home } from './home';
 import { menu } from './menu';
 
-menu();
+
+var homeButton = document.querySelector('#Home');
+var menuButton = document.querySelector('#Menu');
+let content = document.querySelector('.content');
+
+home();
+
+// Now you can add event listeners or manipulate these buttons
+homeButton.addEventListener('click', function() {
+  content.innerHTML = '';
+  home();
+});
+
+menuButton.addEventListener('click', function() {
+  content.innerHTML = '';
+  menu();
+});
 
 //home(); 
 
